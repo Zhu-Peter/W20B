4776bbc9a78d67f15352ba2007f155d7fec0b48e
Zhu-Peter <gzhu@ualberta.ca>
2024-07-22 18:41:13 -0600

    fixed errors

--- queries.sql
+++ queries.sql
@@ -6,11 +6,24 @@
     -- joined_on
 -- Hint, when creating this column if you give a default value of now() it will automatically give this a value of todays date
 create table client (
-    id primary key not null auto_increment,
+    id int(11) not null auto_increment,
     username varchar(255) unique not null,
     password varchar(255) not null,
-    joined_on timestamp not null default now()
-
+    joined_on timestamp not null default now(),
+    primary key (id)
+);
+-- move
+    -- id (PK)
+    -- name
+    -- lower_damage_range
+    -- upper_damage_range
+create table move (
+    id int(11) not null auto_increment,
+    name varchar(255) not null,
+    lower_damage_range int not null,
+    upper_damage_range int not null,
+    primary key (id)
+);
 -- fighter
     -- id (PK)
     -- client_id (FK)
@@ -22,7 +35,7 @@ create table client (
     -- health
     -- points
 create table fighter (
-    id primary key not null auto_increment,
+    id int(11) not null auto_increment,
     client_id int not null,
     move_one int not null,
     move_two int not null,
@@ -31,12 +44,13 @@ create table fighter (
     name varchar(255) not null,
     health int not null,
     points int not null,
+    primary key (id),
     foreign key (client_id) references client(id) on delete cascade,
     foreign key (move_one) references move(id) on delete cascade,
     foreign key (move_two) references move(id) on delete cascade,
     foreign key (move_three) references move(id) on delete cascade,
     foreign key (move_four) references move(id) on delete cascade
-
+);
 -- computer_fighter
     -- id (PK)
     -- move_one (FK)
@@ -46,28 +60,19 @@ create table fighter (
     -- name
     -- health
 create table computer_fighter (
-    id primary key not_null auto_increment,
+    id int(11) not null auto_increment,
     move_one int not null,
     move_two int not null,
     move_three int not null,
     move_four int not null,
     name varchar(255) not null,
     health int not null default 100,
+    primary key (id),
     foreign key (move_one) references move(id) on delete cascade,
     foreign key (move_two) references move(id) on delete cascade,
     foreign key (move_three) references move(id) on delete cascade,
     foreign key (move_four) references move(id) on delete cascade
-
--- move
-    -- id (PK)
-    -- name
-    -- lower_damage_range
-    -- upper_damage_range
-create table move (
-    id primary key not_null auto_increment,
-    name varchar(255) not null,
-    lower_damage_range int not null,
-    upper_damage_range int not null
+);
 
 -- Insert at least 10 moves into your DB
 insert into move (name, lower_damage_range, upper_damage_range) values 
@@ -86,7 +91,7 @@ insert into move (name, lower_damage_range, upper_damage_range) values
 insert into computer_fighter (name, health, move_one, move_two, move_three, move_four) values
     ('Bob', 100, 1, 2, 3, 4),
     ('Chuck', 100, 5, 6, 7, 8),
-    ('Hulk', 1000, 9, 8, 7, 0)
+    ('Hulk', 1000, 9, 8, 7, 10);
 
 
 -- PROCEDURES
@@ -94,27 +99,29 @@ insert into computer_fighter (name, health, move_one, move_two, move_three, move
 -- One that takes in a username and password and adds a new client
 create procedure add_client(user varchar(255), pass varchar(255))
     insert into client (username, password) values (user, pass)
-
+;
 -- One that takes in a username and password and returns the potential user with the matching username and password
 create procedure login(user varchar(255), pass varchar(255))
     select * from client where username = user and password = pass
-
+;
 -- One that takes in a client_id, 4 move id's, fighter name and adds a new fighter
 create procedure add_fighter(client_id int, move_one int, move_two int, move_three int, move_four int, name varchar(255))
     insert into computer_fighter (client_id, move_one, move_two, move_three, move_four, name, health) values (client_id, move_one, move_two, move_three, move_four, name)
-
+;
 -- One that takes in a client_id and returns all fighters that belong to that client
 create procedure get_fighters(client_id int)
     select * from computer_fighter where client_id = client_id
-
+;
 -- One that returns all move id's, names, lower_damage_range and upper_damage_range
 create procedure get_moves()
     select * from moves
-
+;
 -- One that takes in an id for a fighter and a point number and adds to their points total
 create procedure add_points(id int, points int)
     update computer_fighter set computer_fighter.points = computer_fighter.points + points where computer_fighter.id = id
-
+;
 -- One that takes 0 arguments and returns all computer_fighters id, name, health, move_one, move_two, move_three, move_four
 create procedure get_all_fighters()
     select * from computer_fighter
+;
+
